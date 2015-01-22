8a221c5892146ebe1f88bc9ef389188337365d05
Paul Klemm <paul.klemm@googlemail.com>
2015-01-22 13:39:15 +0100

    added node server with proper flowjs upload functionality

--- js-html/prototype/app.js
+++ js-html/prototype/app.js
@@ -1,5 +1,18 @@
 (function() {
-var app = angular.module('cube', [ ]);
+var app = angular.module('cube', ['flow']).config(['flowFactoryProvider', function (flowFactoryProvider) {
+  flowFactoryProvider.defaults = {
+    target: 'http://localhost:8880/upload.php',
+    permanentErrors: [404, 500, 501],
+    maxChunkRetries: 1,
+    chunkRetryInterval: 5000,
+    simultaneousUploads: 4
+  };
+  flowFactoryProvider.on('catchAll', function (event) {
+    console.log('catchAll', arguments);
+  });
+  // Can be used with different implementations of Flow.js
+  // flowFactoryProvider.factory = fustyFlowFactory;
+}]);
 
 // Constructor Code
 app.run(function($rootScope, $http) {
