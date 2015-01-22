5d9f4c2652dddfb0698b8c2dfa44231fd61e83c9
Paul Klemm <paul.klemm@googlemail.com>
2015-01-22 17:53:56 +0100

    renamed uploaded file using sha256 hash

--- js-html/prototype-server/server.js
+++ js-html/prototype-server/server.js
@@ -1,11 +1,13 @@
 process.env.TMPDIR = 'tmp'; // to avoid the EXDEV rename error, see http://stackoverflow.com/q/21071303/76173
-var UPLOAD_DIR = 'uploads';
+var UPLOAD_DIR = 'uploads/';
 
 var express = require('express');
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
 var flow = require('./flow-node.js')('tmp');
 var fs = require('fs');
+var crypto = require('crypto');
+var path = require('path');
 var app = express();
 
 // Configure access control allow origin header stuff
@@ -13,6 +15,23 @@ var ACCESS_CONTROLL_ALLOW_ORIGIN = false;
 
 // Host most stuff in the public folder
 app.use(express.static(__dirname + '/../prototype'));
+app.use(express.static(__dirname + '/uploads'));
+
+var getHashFromFile = function(filepath, filename, callback) {
+  var completePath = filepath + filename;
+  // Read the file again and create a hash for it
+  var shasum = crypto.createHash('sha256');
+  var reader = fs.ReadStream(completePath);
+  reader.on('data', function(hash) {
+    shasum.update(hash);
+  });
+
+  reader.on('end', function() {
+    var hash = shasum.digest('hex');
+    // console.log(d + '  ' + filename);
+    callback(hash);
+  });
+}
 
 // The flow logic is adapted from the flow.js Github Repo.
 // See `flow.js/samples/Node.js/README.md` for more details!
@@ -22,9 +41,21 @@ app.post('/upload', multipartMiddleware, function(req, res) {
     console.log('POST', status, original_filename, identifier);
     // Now we have to write the file from the file chunks!
     // Took the code from here: https://github.com/flowjs/flow.js/issues/17#issuecomment-49737531
-    console.log(filename);
-    var stream = fs.createWriteStream(UPLOAD_DIR + '/' + filename);
-    flow.write(identifier, stream);
+    // console.log(status);
+    if (status == 'done') {
+      var filepath = UPLOAD_DIR + filename;
+      var stream = fs.createWriteStream(filepath);
+      flow.write(identifier, stream);
+
+      getHashFromFile(UPLOAD_DIR, filename, function(hash){
+        console.log(hash);
+        var extension = path.extname(filename);
+        fs.rename(UPLOAD_DIR + filename, UPLOAD_DIR + hash + extension, function(err) {
+          if ( err ) console.log('ERROR: ' + err);
+        });
+      });
+    }
+    // TODO: Remove files from tmp folder after file is assembled
     if (ACCESS_CONTROLL_ALLOW_ORIGIN) {
       res.header("Access-Control-Allow-Origin", "*");
     }
