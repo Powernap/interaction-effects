e1741df50eee6cb0e574766024eb1e625af03213
Paul Klemm <paul.klemm@googlemail.com>
2015-01-23 14:43:38 +0100

    styled file upload section

--- js-html/prototype/app.js
+++ js-html/prototype/app.js
@@ -38,6 +38,13 @@ app.directive('fileUpload', function(){
     templateUrl: 'directives/file-upload.html',
     controller: function($scope){
       this.visible = true;
+      this.uploadEnabled = false;
+      // Has to be defined on the Scope, because `input`s don't have an angular change event
+      // See https://stackoverflow.com/questions/17922557/angularjs-how-to-check-for-changes-in-file-input-fields
+      uploader = this;
+      $scope.fileNameChanged = function(){
+        uploader.uploadEnabled = true;
+      };
 
       $scope.uploader = {};
       this.upload = function() {
