ccff1ccaf589762bb2a6830a58f2588459247484
Paul Klemm <paul.klemm@googlemail.com>
2015-01-29 14:06:29 +0100

    restructured code, moved directive

--- js-html/prototype/app.js
+++ js-html/prototype/app.js
@@ -100,50 +100,6 @@ app.config(['flowFactoryProvider', function (flowFactoryProvider) {
   });
 }]);
 
-app.directive('fileUpload', ['$rootScope', 'createHeatmap', 'rSessions', 'dataLoading', function($rootScope, createHeatmap, rSessions, dataLoading){
-  return {
-    restrict: 'E',
-    templateUrl: 'directives/file-upload.html',
-    controller: function($scope){
-      this.visible = true;
-      this.uploadEnabled = false;
-      this.progressbar = {
-        "visible": false,
-        "percent": 0
-      }
-      this.progressbarVisible = false;
-      var controllerSelf = this;
-      // Has to be defined on the Scope, because `input`s don't have an angular change event
-      // See https://stackoverflow.com/questions/17922557/angularjs-how-to-check-for-changes-in-file-input-fields
-      uploader = this;
-      $scope.fileNameChanged = function(){
-        uploader.uploadEnabled = true;
-      };
-
-      $scope.uploader = {};
-      this.upload = function() {
-        $scope.uploader.flow.upload();
-      };
-
-      $scope.uploader.flowUploadStart = function(){
-        controllerSelf.progressbar.visible = true;
-      };
-
-      $scope.uploader.flowFileProgress = function($file){
-        controllerSelf.progressbar.percent = $file.progress() * 100;
-      };
-
-      $scope.uploader.flowFileSuccess = function ($flow, $file, $message) {
-        // console.log($file);
-        // rSessions.loadDataset(document.URL + $file.name);
-        dataLoading.loadData(document.URL + $file.name);
-        // createHeatmap.createHeatmap().then(function(heatmap){ });
-      };
-    },
-    controllerAs: 'myUploader'
-  };
-}]);
-
 app.factory('createHeatmap', ['$rootScope', '$q', 'dataLoading', function($rootScope, $q, dataLoading) {
 
   var createHeatmapService = {};
