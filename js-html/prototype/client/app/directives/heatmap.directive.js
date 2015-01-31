aa0cac5af85bf7b74b1e5672e8e4e5be45e29bce
Paul Klemm <paul.klemm@googlemail.com>
2015-01-31 20:15:02 +0100

    added pulse effect as incomming data

--- js-html/prototype/client/app/directives/heatmap.directive.js
+++ js-html/prototype/client/app/directives/heatmap.directive.js
@@ -6,6 +6,9 @@ angular.module('cube')
     controller: function($scope){
       var heatmapController = this;
       this.dependent = undefined;
+      // Attach Pulse
+      var pulse = new RCUBE.Pulse('#heatmap-pulse-container');
+
       heatmapController.visible = false;
       // http://jimhoskins.com/2012/12/17/angularjs-and-apply.html
       // https://variadic.me/posts/2013-10-15-share-state-between-controllers-in-angularjs.html
@@ -13,7 +16,7 @@ angular.module('cube')
 
       var createHeatmap = function(dependentVariable){
         // Remove old Heatmap container
-        $('.my-heatmap svg').remove();
+        $('svg.heatmap').remove();
         var names = data.dataset.getDimensionNames();
         var rSquared = data.dataset._rSquared[dependentVariable];
         myHeatmap = new RCUBE.Heatmap(".my-heatmap", rSquared, names);
@@ -25,7 +28,6 @@ angular.module('cube')
       $scope.dependentSelect = this.dependentOptions[0];
       $scope.rSquaredValues = data.getRSquaredValues();
       $scope.$watchCollection('rSquaredValues', function(newValue){
-        console.log($scope.dependentSelect);
         var values = Object.keys(newValue);
         // Set Heatmap to visible when we actually have rSquared values to display
         if (values.length > 0) {
@@ -33,11 +35,11 @@ angular.module('cube')
           // Only add last new entry to the select to keep the old ones
           var newEntry = values[values.length - 1]
           heatmapController.dependentOptions.push({label: newEntry, value: newEntry});
+          pulse.pulse();
         }
       });
 
       this.changeDependent = function(){
-        console.log($scope.dependentSelect);
         this.currentDimension = $scope.dependentSelect.label;
         createHeatmap($scope.dependentSelect.label);
       }
