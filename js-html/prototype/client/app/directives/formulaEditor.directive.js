27eb79b9378bb31ca215e829ad78c4434bd0d4db
Paul Klemm <paul.klemm@googlemail.com>
2015-02-02 20:03:20 +0100

    added default popover

--- js-html/prototype/client/app/directives/formulaEditor.directive.js
+++ js-html/prototype/client/app/directives/formulaEditor.directive.js
@@ -5,18 +5,33 @@ angular.module('cube')
       templateUrl: 'app/directives/formula-editor.html',
 
       controller: function($scope) {
+        var editorController = this;
         this.popup = {};
         this.popup.header = 'My Header';
         this.popup.content = 'My Body';
+        this.popup.show = false;
 
-        test = this.popup;
         this.updatePopup = function(name){
-          $('#formula-popover').popover('show');
-          $scope.$apply(editorController.popup.header = name);
-          $scope.$apply(editorController.popup.content = name);
+          console.log(name);
+          if (name !== undefined){
+            editorController.popup.header = name;
+            // $scope.$apply(editorController.popup.content = name);
+            editorController.popup.content = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+            $scope.$apply();
+          }
+          else {
+            // TODO: Hier weitermachen. Default-Tooltup in watchcollection definieren
+            // TODO: HTML Inject machen
+            var dimensions = data.dataset.getDimensionNames();
+            var dimensionString = '';
+            dimensions.forEach(function(dimension) {
+              dimensionString = dimensionString + dimension + ', ';
+            });
+            editorController.popup.header = 'Available commands';
+            editorController.popup.content = dimensionString.toString();
+            $scope.$apply();
+          }
         };
-
-        var editorController = this;
 
         // Watch the dimension array
         $scope.dimensions = data.dataset.getDimensionNames();
